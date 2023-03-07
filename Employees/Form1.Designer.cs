37714c86c2b78b8e733cb6469de8e44f57b6674e
Koleto <twindestroyersbgbg@gmail.com>
2023-03-07 21:32:54 +0200

    Add DateTime

--- Employees/Form1.Designer.cs
+++ Employees/Form1.Designer.cs
@@ -29,25 +29,43 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(93, 449);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(206, 333);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(108, 46);
+            this.button1.Size = new System.Drawing.Size(135, 47);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Choose file";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(55, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(444, 127);
+            this.dataGridView1.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(756, 569);
+            this.ClientSize = new System.Drawing.Size(607, 443);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Pair of employees who have worked together";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -55,6 +73,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Employees/Form1.cs
+++ Employees/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,58 @@ namespace Employees
         public Form1()
         {
             InitializeComponent();
+        }
+        DataTable table = new DataTable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var loadDialog = new OpenFileDialog { Filter = "Text File|*.txt" };
+            loadDialog.ShowDialog();
+            string[] lines = File.ReadAllLines($@"{loadDialog.FileName}");
+            string[] data;
+
+            table.Columns.Add("Employee ID #1", typeof(string));
+            table.Columns.Add("Employee ID #2", typeof(string));
+            table.Columns.Add("Project ID", typeof(string));
+            table.Columns.Add("Days Worked", typeof(string));
+            Dictionary<int,int> projId = new Dictionary<int, int>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                data = lines[i].ToString().Split(',');
+                DateTimeOffset dateStart = new DateTimeOffset();
+                DateTimeOffset dateEnd = new DateTimeOffset();
+                dateStart = DateTimeOffset.Parse(data[2]);
+                if (data[3] != "NULL")
+                {
+                    dateEnd = DateTimeOffset.Parse(data[3]);
+                }
+                else
+                {
+                    dateEnd = DateTimeOffset.UtcNow;
+                }
+                TimeSpan timeSpan = dateEnd - dateStart;
+                int days = timeSpan.Days;
+               
+                if (!projId.ContainsKey(int.Parse(data[1])))
+                {
+                    projId.Add(int.Parse(data[1]), days);
+                }
+                else
+                {
+                    projId[int.Parse(data[1])] += days;
+                }
+                string[] row = new string[data.Length];
+                for (int j = 0; j < data.Length; j++)
+                {
+                    row[j] = data[j].Trim();
+                }
+                table.Rows.Add(row);
+            }
+            dataGridView1.DataSource = table;
         }
     }
 }
