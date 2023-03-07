186c3af2c091212d69d6e8645dd49aab032e34bf
Koleto <twindestroyersbgbg@gmail.com>
2023-03-07 22:38:58 +0200

    Add Formats

--- Employees/Form1.Designer.cs
+++ Employees/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -37,10 +36,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(206, 333);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(275, 410);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 47);
+            this.button1.Size = new System.Drawing.Size(180, 58);
             this.button1.TabIndex = 0;
             this.button1.Text = "Choose file";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,20 +47,26 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(55, 67);
+            this.dataGridView1.Location = new System.Drawing.Point(73, 82);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(444, 127);
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(608, 91);
             this.dataGridView1.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(607, 443);
+            this.ClientSize = new System.Drawing.Size(780, 545);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Pair of employees who have worked together";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -73,7 +78,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Employees/Form1.cs
+++ Employees/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -36,15 +37,17 @@ namespace Employees
             table.Columns.Add("Project ID", typeof(string));
             table.Columns.Add("Days Worked", typeof(string));
             Dictionary<int,int> projId = new Dictionary<int, int>();
+            string[] formats = { "MM/dd/yyyy", "yyyy-MM-dd","MMMM dd", "dddd, dd MMMM yyyy","yyyy MMMM"};
             for (int i = 0; i < lines.Length; i++)
             {
                 data = lines[i].ToString().Split(',');
                 DateTimeOffset dateStart = new DateTimeOffset();
                 DateTimeOffset dateEnd = new DateTimeOffset();
-                dateStart = DateTimeOffset.Parse(data[2]);
-                if (data[3] != "NULL")
+                
+                dateStart = DateTimeOffset.ParseExact(data[2].Trim(),formats,new CultureInfo("en-GB"),DateTimeStyles.None);
+                if (data[3].Trim() != "NULL")
                 {
-                    dateEnd = DateTimeOffset.Parse(data[3]);
+                    dateEnd = DateTimeOffset.ParseExact(data[3].Trim(), formats, new CultureInfo("en-GB"), DateTimeStyles.None);
                 }
                 else
                 {
@@ -53,21 +56,44 @@ namespace Employees
                 TimeSpan timeSpan = dateEnd - dateStart;
                 int days = timeSpan.Days;
                
-                if (!projId.ContainsKey(int.Parse(data[1])))
+                if (!projId.ContainsKey(int.Parse(data[1].Trim())))
                 {
-                    projId.Add(int.Parse(data[1]), days);
+                    projId.Add(int.Parse(data[1].Trim()), days);
                 }
                 else
                 {
-                    projId[int.Parse(data[1])] += days;
+                    projId[int.Parse(data[1].Trim())] += days;
                 }
-                string[] row = new string[data.Length];
-                for (int j = 0; j < data.Length; j++)
-                {
-                    row[j] = data[j].Trim();
-                }
-                table.Rows.Add(row);
             }
+            int max = 0;
+            int keyMax = 0;
+            foreach(KeyValuePair<int, int> kvp in projId)
+            {
+                if (projId[kvp.Key] > max)
+                {
+                    max = projId[kvp.Key];
+                    keyMax = kvp.Key;
+                }
+            }
+            bool firstPassed = false;
+            string[] row = new string[4];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                data = lines[i].ToString().Split(',');
+                if (data[1].Trim() == keyMax.ToString() && firstPassed==false)
+                {
+                    row[0] = data[0];
+                    firstPassed = true;
+                }
+                else if (data[1].Trim() == keyMax.ToString())
+                {
+                    row[1] = data[0];
+                    break;
+                }
+            }
+            row[2] = keyMax.ToString();
+            row[3] = max.ToString();
+            table.Rows.Add(row);
             dataGridView1.DataSource = table;
         }
     }
